a0ee1ce194cef7adde99bf587d0f102d822972cb
The-Kevin <kevin.almeida1111.ka@gmail.com>
2022-03-05 18:52:29 -0300

    feat: validations

--- src/modules/routes.ts
+++ src/modules/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import transactionRoutes from './transactions/routes';
 const routes = Router();
 
+routes.use('/transactions', transactionRoutes);
 export default routes;
